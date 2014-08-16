17ed38e23a5c0f82e8efbe681cae655433df14c5
oviedomonica123 <oviedo.monica123@gmail.com>
2014-08-16 15:13:19 -0430

    Create secret_token.rb

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
++# Be sure to restart your server when you modify this file.
++
++# Your secret key is used for verifying the integrity of signed cookies.
++# If you change this key, all old signed cookies will become invalid!
++
++# Make sure the secret is at least 30 characters and all random,
++# no regular words or you'll be exposed to dictionary attacks.
++# You can use `rake secret` to generate a secure secret key.
++
++# Make sure your secret_key_base is kept private
++# if you're sharing your code publicly.
++ConestEncuestaBackend::Application.config.secret_key_base = '22cd4074cb271cb44f479d85fffea061d078f9db00b0fb16382c2652b35b143b0d2b104645bf41cf3fcfdf881ec7d13fcd98959031e3cf6459bd55fb1aac444e'
